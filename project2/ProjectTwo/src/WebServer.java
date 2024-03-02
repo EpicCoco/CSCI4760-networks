4a25486cbd87230454f6789a1d62e136cde96fef
EpicCoco <forthecodeman@gmail.com>
2024-03-02 18:41:40 -0500

    Fixed file structure

--- project2/ProjectTwo/src/WebServer.java
+++
@@ -1,8 +0,0 @@
-import java.io.* ;
-import java.net.* ;
-import java.util.* ;
-public final class WebServer {
-public static void main(String args[]) throws Exception {
-
-    } //main
-} //WebServer
