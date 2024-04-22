ae6ee4ce68b8e968895437616de883142345a3dc
EpicCoco <forthecodeman@gmail.com>
2024-04-22 15:59:53 -0400

    Finish project, add readme

---
+++ project3/README.md
@@ -0,0 +1,8 @@
+# Codey Borrelli - crb70853
+
+To run:
+cd project3
+javac -d bin .\src\TracerouteParser.java
+java -cp bin TracerouteParser
+
+Results are printed to standard out
--- project3/src/TracerouteParser.java
+++ project3/src/TracerouteParser.java
@@ -3,10 +3,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TracerouteParser {
 
@@ -15,25 +13,30 @@ public class TracerouteParser {
         List<String> results = parseTrace(traceFile);
         for (String result : results) {
             System.out.println(result);
-        }
-    }
+        } //for
+    } //main
 
     public static List<String> parseTrace(String traceFile) {
-        DecimalFormat df = new DecimalFormat("#.###");
+        DecimalFormat df = new DecimalFormat("0.000");
         List<String> result = new ArrayList<>();
-        int currentTTL = 0;
+        int numLines = 0;
         HashMap<Integer, Double> idToTime = new HashMap<>(); 
         HashMap<String, ArrayList<Double>> ipToTime = new HashMap<>();
+        HashMap<Double, Integer> timeToTTL = new HashMap<>();
+        HashMap<Integer, String> TTLtoIp = new HashMap<>();
         
         try (BufferedReader br = new BufferedReader(new FileReader(traceFile))) {
             String line;
             while ((line = br.readLine()) != null) {
+                numLines++;
                 if (line.contains("proto TCP")) {
                     String line2 = br.readLine();
                     String[] parsed = line.split(" ");
                     double time = Double.parseDouble(parsed[0]);
                     int id = Integer.parseInt(parsed[7].substring(0, parsed[7].length() - 1));
+                    int TTL = Integer.parseInt(parsed[5].substring(0, parsed[5].length() - 1));
                     idToTime.put(id, time);
+                    timeToTTL.put(time, TTL);
                 } else if (line.contains("proto ICMP")) {
                     String line2 = br.readLine();
                     String line3 = br.readLine();
@@ -43,34 +46,36 @@ public class TracerouteParser {
                     double time = Double.parseDouble(parsed1[0]);
 
                     String[] parsed2 = line2.split(" ");
-                    //System.out.println(Arrays.toString(parsed2));
                     String ip = parsed2[4];
 
                     String[] parsed3 = line3.split(" ");
                     int id = Integer.parseInt(parsed3[6].substring(0, parsed3[6].length() - 1));
 
-                    double timeDiff = Double.parseDouble(df.format((time - idToTime.get(id)) * 1000));
+                    double timeDiff = (time - idToTime.get(id)) * 1000;
                     if (!ipToTime.containsKey(ip)) ipToTime.put(ip, new ArrayList<>());
                     ArrayList<Double> temp = ipToTime.get(ip);
                     temp.add(timeDiff);
+                    TTLtoIp.put(timeToTTL.get(idToTime.get(id)), ip);
+                    
                 } //if
             } //while
 
-            System.out.println("PRINTING RESULT");
-            for (Map.Entry<String, ArrayList<Double>> entry : ipToTime.entrySet()) {
-                String ip = entry.getKey();
-                ArrayList<Double> times = entry.getValue();
-                System.out.println("TTL");
-                System.out.println(ip);
+            //printing
+            for (int i = 0; i < numLines; i++) {
+                if (!TTLtoIp.containsKey(i)) continue;
+                result.add("TTL " + i);
+                String ip = TTLtoIp.get(i);
+                result.add(ip);
+                ArrayList<Double> times = ipToTime.get(ip);
                 for (double time : times) {
-                    System.out.println(time + " ms");
+                    result.add(df.format(time) + " ms");
                 } //for
             } //for
-            
+
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } //try
 
         return result;
-    }
-}
+    } //parseTrace
+} //TracerouteParser
